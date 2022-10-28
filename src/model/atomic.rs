5505fe3e384fe67b36dd1527199e9fc980d93808
Román Cárdenas <rcardenas.rod@gmail.com>
2022-10-28 17:48:49 +0200

    Changes in AsAtomic

--- src/model/atomic.rs
+++ src/model/atomic.rs
@@ -3,51 +3,52 @@ use crate::{AsModel, Model};
 
 impl<T: AsAtomic> AsModel for T {
     fn as_model(&self) -> &Model {
-        self.get_model()
+        AsAtomic::as_model(self)
     }
 
     fn as_model_mut(&mut self) -> &mut Model {
-        self.get_model_mut()
+        AsAtomic::as_model_mut(self)
     }
 
     fn start_simulation(&mut self, t_start: f64) {
         let ta = self.ta();
-        self.get_model_mut().set_clock(t_start, t_start + ta);
+        AsAtomic::as_model_mut(self).set_clock(t_start, t_start + ta);
     }
 
     fn stop_simulation(&mut self, t_stop: f64) {
-        self.get_model_mut().set_clock(t_stop, f64::INFINITY);
+        AsAtomic::as_model_mut(self).set_clock(t_stop, f64::INFINITY);
     }
 
     fn lambda(&mut self, t: f64) {
-        if t >= self.get_model().clock.t_next {
+        if t >= AsAtomic::as_model(self).get_t_next() {
             AsAtomic::lambda(self);
         }
     }
 
     fn delta(&mut self, t: f64) {
+        let t_next = AsAtomic::as_model_mut(self).get_t_next();
         if !self.is_input_empty() {
-            if t == self.get_model().clock.t_next {
+            if t == t_next {
                 self.delta_conf();
             } else {
-                let e = t - self.get_model().clock.t_last;
+                let e = t - self.get_time();
                 self.delta_ext(e);
             }
-        } else if t == self.get_model().clock.t_next {
+        } else if t == t_next {
             self.delta_int();
         } else {
             return;
         }
         let ta = self.ta();
-        self.get_model_mut().set_clock(t, t + ta)
+        AsAtomic::as_model_mut(self).set_clock(t, t + ta)
     }
 }
 
 /// Interface for atomic DEVS models.
 pub trait AsAtomic: Debug {
-    fn get_model(&self) -> &Model;
+    fn as_model(&self) -> &Model;
 
-    fn get_model_mut(&mut self) -> &mut Model;
+    fn as_model_mut(&mut self) -> &mut Model;
 
     /// Output function of the atomic DEVS model.
     fn lambda(&self);
@@ -77,11 +78,10 @@ pub trait AsAtomic: Debug {
 #[macro_export]
 macro_rules! impl_atomic {
     ($($ATOMIC:ident),+) => {
-        // use $crate::{AbstractSimulator, AsAtomic, AsModel};
         $(
             impl AsAtomic for $ATOMIC {
-                fn get_model(&self) -> &Model { &self.model }
-                fn get_model_mut(&mut self) -> &mut Model { &mut self.model }
+                fn as_model(&self) -> &Model { &self.model }
+                fn as_model_mut(&mut self) -> &mut Model { &mut self.model }
                 fn lambda(&self) { self.lambda(); }
                 fn delta_int(&mut self) { self.delta_int() }
                 fn delta_ext(&mut self, e: f64) { self. delta_ext(e) }
