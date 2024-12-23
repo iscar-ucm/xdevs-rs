af711937c1d98d3be0fd123787769a41ca62c28f
Román Cárdenas Rodríguez <rcardenas.rod@gmail.com>
2024-12-23 11:32:11 +0100

    RT capabilities

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,8 +1,12 @@
-use crate::modeling::{Atomic, Component, Coupled};
-use crate::DynRef;
+use crate::{
+    modeling::{Atomic, Component, Coupled},
+    DynRef,
+};
 #[cfg(feature = "par_any")]
 use rayon::prelude::*;
 use std::ops::{Deref, DerefMut};
+
+pub mod rt;
 
 /// Interface for simulating DEVS models. All DEVS models must implement this trait.
 pub trait Simulator: DynRef {
@@ -36,12 +40,14 @@ pub trait Simulator: DynRef {
         self.get_component_mut().set_sim_t(t_last, t_next);
     }
 
+    /// Clears input messages from the inner DEVS [`Component`]s.
     #[inline]
     fn clear_input(&mut self) {
         // Safety: simulator clearing its input
         unsafe { self.get_component_mut().clear_input() };
     }
 
+    /// Clears output messages from the inner DEVS [`Component`]s.
     #[inline]
     fn clear_output(&mut self) {
         // Safety: simulator clearing its output
@@ -140,6 +146,9 @@ impl Simulator for Coupled {
     /// method and obtain the next simulation time.
     ///
     /// If the feature `par_start` is activated, the iteration is parallelized.
+    ///
+    /// If the feature `par_couplings` is activated, the EICs, EOCs, and ICs are built
+    /// for enabling parallel event propagation.
     fn start(&mut self, t_start: f64) -> f64 {
         #[cfg(feature = "par_start")]
         let iter = self.components.par_iter_mut();
@@ -178,10 +187,10 @@ impl Simulator for Coupled {
     }
 
     /// Iterates over all the subcomponents to call their [`Simulator::collection`] method.
-    /// If the feature `par_collection` is activated, the iteration is parallelized.
-    /// Then, it iterates over all the EOCs and propagates messages accordingly.
+    /// Then, it iterates over all the EOCs and ICs and propagates messages accordingly.
     ///
-    /// If the feature `par_eoc` is activated, the iteration is parallelized.
+    /// If the feature `par_collection` is activated, the iteration over subcomponents is parallelized.
+    /// If the feature `par_couplings` is activated, the iteration is over couplings is parallelized.
     fn collection(&mut self, t: f64) {
         if t >= self.get_t_next() {
             #[cfg(feature = "par_collection")]
@@ -212,14 +221,14 @@ impl Simulator for Coupled {
         }
     }
 
-    /// Iterates over all the EICs and ICs and propagates messages accordingly.
-    /// If the feature `par_xic` is activated, the iteration is parallelized.
+    /// Iterates over all the EICs and propagates messages accordingly.
     /// Then, it iterates over all the subcomponents to:
     /// 1. Call their [`Simulator::transition`] method
     /// 2. Clear their ports
-    /// 3. obtain their next simulation time.
+    /// 3. Obtain their next simulation time.
     ///
-    /// If the feature `par_transition` is activated, the iteration is parallelized.
+    /// If the feature `par_couplings` is activated, the iteration over EICs is parallelized.
+    /// If the feature `par_transition` is activated, the iteration over subcomponents is parallelized.
     fn transition(&mut self, t: f64) -> f64 {
         // Safety: simulator checking if its input is empty
         let is_external = !unsafe { self.get_component().is_input_empty() };
@@ -260,6 +269,7 @@ impl Simulator for Coupled {
 }
 
 /// Root coordinator for sequential simulations of DEVS models.
+#[repr(transparent)]
 pub struct RootCoordinator<T>(T);
 
 impl<T: Simulator> RootCoordinator<T> {
@@ -269,13 +279,65 @@ impl<T: Simulator> RootCoordinator<T> {
     }
 
     /// Runs a simulation for a given period of time.
-    pub fn simulate(&mut self, t_end: f64) {
+    pub fn simulate(&mut self, t_stop: f64) {
         let mut t_next = self.start(0.);
-        while t_next < t_end {
+        while t_next < t_stop {
             self.collection(t_next);
             t_next = self.transition(t_next);
         }
         self.stop(t_next);
+    }
+
+    /// Runs a Real-Time (RT) simulation for a given period of time.
+    ///
+    /// # The `wait_event` closure
+    ///
+    /// RT behavior must be implemented in the `wait_event` closure.
+    /// The `wait_event` closure behaves as a function that accepts two arguments:
+    ///
+    /// - `t_next: f64` - The next simulation time in which a model state transition is expected.
+    /// - `component: &T` - A reference to the model under study.
+    ///
+    /// It must return the simulation time when the next event happens.
+    ///
+    ///  If no external event is received, the `wait_event` closure waits until the next internal
+    /// event and returns `t_next`, which now is the current simulation time. We provide an
+    /// [`rt::sleep`] closure that can be used as a `wait_event` closure for this scenario.
+    ///
+    /// When external events may occur, the behavior of the `wait_event` closure is more complex.
+    /// Now, the `wait_event` closure must wait for external events without exceeding the time for
+    /// the next internal event. If an external event is received, the `wait_event` closure must
+    /// inject the events into the model's input ports and return the simulation time when the event
+    /// was received. We provide an [`rt::wait_event`] closure that can be used as a `wait_event`
+    ///
+    /// Under no circumstances should the `wait_event` closure return a value higher than `t_next`.
+    ///
+    /// # The `output_handler` closure
+    ///
+    /// The `output_handler` closure behaves as a function that accepts a reference to the model
+    /// under study and propagates the output messages to external entities.
+    /// Every time the model  generates output messages, the `output_handler` closure is called.
+    /// Implementation details of the `output_handler` closure depend on the specific use case.
+    pub fn simulate_rt(
+        &mut self,
+        t_stop: f64,
+        mut wait_event: impl FnMut(f64, &T) -> f64,
+        mut output_handler: impl FnMut(&T),
+    ) {
+        let mut t = 0.;
+        let mut t_next_internal = self.start(t);
+        while t < t_stop {
+            let t_until = f64::min(t_next_internal, t_stop);
+            t = wait_event(t_until, self);
+            if t >= t_next_internal {
+                self.collection(t);
+                output_handler(self);
+            } else if unsafe { self.get_component().is_input_empty() } {
+                continue; // avoid spurious external transitions
+            }
+            t_next_internal = self.transition(t);
+        }
+        self.stop(t_stop);
     }
 }
 
